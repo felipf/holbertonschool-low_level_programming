c64935005d449962f83ab517610aad2834fe058c
felipf <6341@holbertonstudents.com>
2023-02-16 05:25:35 -0800

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- *  main - will print a string
- *   
- *     Return: Always 0 (Success)
+/**
+ * main - will print a string
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
