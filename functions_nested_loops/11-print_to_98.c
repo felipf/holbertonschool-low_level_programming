60e2b4403907b9d0271208cea093f09a250d12cd
felipf <6341@holbertonstudents.com>
2023-02-17 09:12:16 -0800

    task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - results going from 98 or up to 98.
+ * @n: num1.
+ * Return: the result.
+ */
+void print_to_98(int n)
+{
+	int i;
+	
+	if (n == 98)
+	{
+		printf("%d", n);
+		putchar('\n');
+	}
+	else if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+}
