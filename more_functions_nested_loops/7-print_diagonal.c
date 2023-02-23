ea688a7c5c5265ce4bbf7334b9ecb911c655672f
felipf <6341@holbertonstudents.com>
2023-02-23 08:31:15 -0800

    task7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line
+ *
+ * @n: number of times it prints.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n == 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(92);
+			_putchar(10);
+			if (i != n)
+			{
+				for (j = 0; j < i; j++)
+					_putchar(32);
+			}
+		}
+	}
+}
