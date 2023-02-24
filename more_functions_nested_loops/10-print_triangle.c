b2ddb29376d1044b605ff00be5dad68cd85a02d1
felipf <6341@holbertonstudents.com>
2023-02-24 10:06:36 -0800

    task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle.
+ *
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int s, z, c;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (s = 1; s <= size; s++)
+		{
+			for (z = size; z > s; z--)
+			{
+				_putchar(32);
+			}
+			for (c = 1; c <= z; c++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+}
