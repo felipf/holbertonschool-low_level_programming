239bebe4e16426161b9affb4b8a4e42f66a4b0a6
felipf <6341@holbertonstudents.com>
2023-02-28 04:24:47 -0800

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string, to stdout
+ * @str: the string.
+ * Return: the result.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
