90ce643787b4b7f83e8bafd7b4146dcfdaef4b3f
felipf <6341@holbertonstudents.com>
2023-03-05 07:48:28 -0800

    task4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - search a sring for any set of bytes
+ * @s: the string
+ * @accept: to find
+ * Return: s or null.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
