8e3896c00468d5846355dd2402ec04e6fd5f8fd7
felipf <6341@holbertonstudents.com>
2023-03-06 05:50:23 -0800

    task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * _strlen_recursion - length of a string
  * @s: string
@@ -7,11 +8,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	if (*s == '\0')
+		return (0);
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	return 1 + _strlen_recursion(s + 1);
 }
