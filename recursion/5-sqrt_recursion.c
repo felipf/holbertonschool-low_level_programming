45cfa631a789452522bc86a1ce964d342c8e5dad
felipf <6341@holbertonstudents.com>
2023-03-07 04:50:37 -0800

    task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _sqrt_recursion - root of a number
+ * @n: number
+ * Return: square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 0));
+}
+/**
+ * _sqrt - calculates the square root of a number and returns it
+ * @n: number
+ * @i: square root
+ * Return: root of number or -1 if not found
+ */
+int _sqrt(int n, int i)
+{
+	return ((i * i == n) ? i : (i * i > n) ? -1 : _sqrt(n, i + 1));
+}
