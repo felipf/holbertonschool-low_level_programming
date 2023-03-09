109d3b95bc5bd0c008cebdc06046bb45bfe66653
felipf <6341@holbertonstudents.com>
2023-03-09 09:47:30 -0800

    task 0

---
+++ static_libraries/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+/**
+ * _atoi - converts a string to an integer.
+ * @s: the string
+ * Return: The integer representation of the string
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int digit;
+
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+		sign *= -1;
+		}
+		else if (*s == '+')
+		{
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit = *s - '0';
+
+
+		if (result > (INT_MAX - digit) / 10)
+		{
+			return (sign > 0 ? INT_MAX : INT_MIN);
+		}
+			result = result * 10 + digit;
+		}
+			else if (result > 0)
+		{
+
+
+			break;
+		}
+		s++;
+	}
+	return (sign * result);
+}
