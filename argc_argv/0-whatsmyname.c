9158c5326380803fc9b7ebca9dad118da1661f4f
felipf <6341@holbertonstudents.com>
2023-03-12 13:48:38 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - name followed by a new line.
+ * @argc: n entered in cmd line
+ * @argv: add string values entered
+ * Return: 0.
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
