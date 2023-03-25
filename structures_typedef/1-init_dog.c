7bfaf24fb3645ec33b02cdb6f46789d3304206ba
felipf <6341@holbertonstudents.com>
2023-03-25 14:57:47 -0700

    tasks

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - initialize a var type struct dog.
+ * @d: dog.
+ * @name: the name.
+ * @age: the age.
+ * @owner: the owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints a struct dog.
+ * @d: dog.
+ */
+void print_dog(struct dog *d)
+{
+	char *nil = "(nil)";
+
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : nil);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : nil);
+	}
+}
