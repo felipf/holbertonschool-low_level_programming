d0cd4982793e674fb14c6d2ffd1be290f1c71730
felipf <6341@holbertonstudents.com>
2023-03-26 19:46:14 -0700

    tasks

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - func that exec a func given as a parameter on each element of an array
+ * @array: the array
+ * @size: size of array
+ * @action: function
+ */
+void array_interator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	if (array && action)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
