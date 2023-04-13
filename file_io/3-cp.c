650ea238076acaf58debc9c67b75e1854383f117
felipf <6341@holbertonstudents.com>
2023-04-13 06:28:39 -0700

    Tasks

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	copy_text(argv[1], argv[2]);
+	cp_content(argv[1], argv[2]);
 	exit(0);
 }
 /**
@@ -29,19 +29,19 @@ void cp_content(const char *file_one, const char *file_two)
 	file_from = open(file_one, O_RDONLY);
 
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_one), exit(98);
 
 	file_to = open(file_two, O_CREAT | O_TRUNC | O_WRONLY, modsie);
 	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_two), exit(99);
 
 	nread = read(file_from, cCount, 5555);
 	if (nread == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_one), exit(98);
 
-	writeMe = write(file_to, cCount, readMe);
-	if (writeMe == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to), exit(99);
+	nwrite = write(file_to, cCount, nread);
+	if (nwrite == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_two), exit(99);
 
 	if (close(file_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
