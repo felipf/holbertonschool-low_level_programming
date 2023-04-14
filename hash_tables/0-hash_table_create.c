ad5f24fb111fcb0cce1dc7df9dfc3eae2d051795
felipf <6341@holbertonstudents.com>
2023-04-14 07:16:57 -0700

    task 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creating hash table
+ * @size: array size
+ * Return: pointer to hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t  *newHashT = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+
+	if (newHashT == NULL)
+		return (NULL);
+
+	newHashT->array = malloc(size * sizeof(hash_node_t *));
+	if (newHashT->array == NULL)
+	{
+		free(newHashT);
+		return (NULL);
+	}
+	for (i= 0; i < size; i++)
+		newHashT->array[i] = NULL;
+
+	newHashT->size = size;
+
+	return (newHashT);
+}
