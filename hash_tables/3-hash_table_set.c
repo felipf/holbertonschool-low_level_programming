e3c939488999e83c831771a3e62896f8ffce6da7
felipf <6341@holbertonstudents.com>
2023-04-14 08:47:58 -0700

    task 3 finish

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,11 +1,39 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * key_index - func gives index of key
- * @key: to find index
- * @size: size of array of hash
- * Return: index which value will be stored in array
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to add or update the key/value to
+ * @key: key
+ * @value: the value assoc. with the key
+ *
+ * Return: 1 for Success
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	return ((hash_djb2(key) % size));
+	hash_node_t *new_node, *temp_node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	/* compute the index of the key in the hash table */
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/* allocate memory for the new node */
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	/* copy the key and value into the new node */
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	/* handle collisions by adding the new node at the beginning of the list */
+	temp_node = ht->array[index];
+	ht->array[index] = new_node;
+	new_node->next = temp_node;
+
+	return (1);
 }
